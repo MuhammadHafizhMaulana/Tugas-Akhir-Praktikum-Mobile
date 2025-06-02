85ac22da57f10b98c3fe37d575c9a16ab0368e08
MuhammadHafizhMaulana <maulanahafizh146@gmail.com>
2025-06-02 23:05:16 +0700

    database buat user udah, bisa login dan register

---
+++ lib/db/database_helper.dart
@@ -0,0 +1,67 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+  factory DBHelper() => _instance;
+  DBHelper._internal();
+
+  Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    final path = join(await getDatabasesPath(), 'users.db');
+    return openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+      )
+    ''');
+  }
+
+  Future<int> insertUser(String name, String email, String password) async {
+    final db = await database;
+    return await db.insert('users', {
+      'name' : name,
+      'email': email,
+      'password': password,
+    });
+  }
+
+  Future<bool> userExists(String email) async {
+    final db = await database;
+    final res = await db.query(
+      'users',
+      where: 'email = ?',
+      whereArgs: [email],
+    );
+    return res.isNotEmpty;
+  }
+
+   Future<Map<String, dynamic>?> getUser(String email, String password) async {
+    final db = await database;
+
+    final result = await db.query(
+      'users',
+      where: 'email = ? AND password = ?',
+      whereArgs: [email, password],
+    );
+
+    if (result.isNotEmpty) {
+      return result.first;
+    } else {
+      return null;
+    }
+  }
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,35 @@
+class User {
+  final int? id;
+  final String name;
+  final String email;
+  final String password;
+  final String createdAt;
+
+  User({
+    this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+      'created_at': createdAt,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'],
+      name: map['name'],
+      email: map['email'],
+      password: map['password'],
+      createdAt: map['created_at'],
+    );
+  }
+}
